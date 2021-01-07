98233a5ef15621cd4d9b542b8a5adf0969ac7503
Emad Albarnawi <Emadk789@hotmail.com>
2021-01-07 13:26:50 +0300

    chore: Initial design of DetailsViewController.

--- TVManager/ViewControllers/Collection View/Horizantal Collection View/DetailsViewController.swift
+++ TVManager/ViewControllers/Collection View/Horizantal Collection View/DetailsViewController.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
     
 
     
+}
+
+//MARK:- UICollectionViewDelegate, UICollectionViewDataSource
+extension DetailsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! DetailsViewControllerCell
+        cell.imageView.backgroundColor = .systemRed
+        cell.backgroundColor = .systemBlue
+        return cell
+    }
+    
+    
+}
+// MARK:- DetailsViewControllerCell
+class DetailsViewControllerCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
+
+extension DetailsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.size.width
+        let screenHeight = UIScreen.main.bounds.size.height
+        return CGSize(width: screenWidth, height: screenHeight)
+    }
 }
